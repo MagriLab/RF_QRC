32d1c3ad401ffaabcb11a606202bbb6f2c91320a
Osama Ahmed <osamaahmed1224@gmail.com>
2024-06-27 16:51:19 +0100

    added target lyapunov calculation from data -lor63

--- src/QRC/solvers.py
+++ src/QRC/solvers.py
@@ -59,6 +59,51 @@ class Solvers:
         return q[self.N_transient+1:] # only return the data after removing transients
 
 
+
+    def F_all(self,eom, jac, u, M):
+
+        ''' solves system. u is the state vector composed
+            by the variables. J is the Jacobian and M are the
+            Gram-Schmidt Lyapunov vectors'''
+
+        dqdt  = eom(u)
+        J     = jac(u)
+        dMdt  = np.dot(J, M)
+
+        return dqdt, dMdt
+
+
+    def RK4var(self,f, eom, jac, x, mat):  # (F_all, eom, jac, xt, dt, params, U)
+
+        K1, M1 = f(eom, jac, x, mat)
+        K2, M2 = f(eom, jac, x + self.dt*K1/2.0, mat + self.dt*M1/2.0)
+        K3, M3 = f(eom, jac, x + self.dt*K2/2.0, mat + self.dt*M2/2.0)
+        K4, M4 = f(eom, jac, x + self.dt*K3, mat + self.dt*M3)
+
+        A = np.array(self.dt * (K1/2.0 + K2 + K3 + K4/2.0) / 3.0)
+        B = np.array(self.dt * (M1/2.0 + M2 + M3 + M4/2.0) / 3.0)
+
+        return A, B
+
+    def qr_factorization(self,A):
+        m, n = A.shape
+        Q = np.zeros((m, n))
+        R = np.zeros((n, n))
+
+        for j in range(n):
+            v = A[:, j].copy()
+
+            for i in range(j):
+                q = Q[:, i]
+                R[i, j] = q.dot(v)
+                v = v - R[i, j] * q
+
+            norm = np.linalg.norm(v)
+            Q[:, j] = v / norm
+            R[j, j] = norm
+        return Q, R
+
+
 def RK4(q0,dt,N,func,N_transient):
     """4th order RK for autonomous systems described by func
 
--- src/QRC/systems.py
+++ src/QRC/systems.py
@@ -4,6 +4,8 @@ import numpy as np
 import h5py
 #from QESN.solvers import *
 from QRC.solvers import Solvers
+import scipy
+
 
 class Systems(Solvers):
 
@@ -61,9 +63,29 @@ class Systems(Solvers):
         #print(self.sigma,self.r,self.b)
         return np.array([(self.sigma)*(y-x), x*(self.r-z)-y, x*y-self.b*z])
 
+
+    def solve_lorenz63_jac(self,u):
+        x, y, z          = u  # Unpack the state vector
+        #Jacobian
+        J = np.array([[-self.sigma, self.sigma,     0],
+                    [ self.r-z,    -1,    -x],
+                    [     y,     x, -self.b]])
+        return J
+
+
     def solve_lorenz96(self,u):
         p = 8
         return np.roll(u,1) * (np.roll(u,-1) - np.roll(u,2)) - u + p
+
+    def solve_lorenz96_jac(self,u):
+        D = len(u)
+        J = np.zeros((D,D), dtype='float')
+        for i in range(D):
+            J[i,(i-1)%D] =  u[(i+1)%D] - u[(i-2)%D]
+            J[i,(i+1)%D] =  u[(i-1)%D]
+            J[i,(i-2)%D] = -u[(i-1)%D]
+            J[i,i] = -1.0
+        return J
 
     def solve_MFE(self,u):
 
@@ -110,6 +132,59 @@ class Systems(Solvers):
 
         return dqdt
 
+    def solve_MFE_jac(self,u):
+        """
+        Defines the jacobian for the differential equations for Moehlis_2004
+
+        Arguments:
+            w :  vector of the state variables a_i
+            p :  vector of the parameters:
+        """
+        a1, a2, a3, a4, a5, a6, a7, a8, a9 = u
+
+        # Problem Definition
+        Lx = 4*math.pi
+        # Ly = 2
+        Lz = 2*math.pi
+        Re = 400
+
+        # Parameter values
+        alfa  = 2*math.pi/Lx
+        beta  = math.pi/2
+        gamma = 2*math.pi/Lz
+
+        k1 = math.sqrt(alfa**2 + gamma**2)
+        k2 = math.sqrt(gamma**2 + beta**2)
+        k3 = math.sqrt(alfa**2 + beta**2 + gamma**2)
+
+        J = np.array([[-beta**2/Re, np.sqrt(3/2)*beta*gamma/k2*a3, np.sqrt(3/2)*beta*gamma/k2*a2, 0., 0., - np.sqrt(3/2)*beta*gamma/k3*a8, 0., - np.sqrt(3/2)*beta*gamma/k3*a6, 0.],
+
+                [- np.sqrt(3/2)*beta*gamma/k2*a3, - (4/3*beta**2 + gamma**2)/Re, - np.sqrt(3/2)*beta*gamma/k2*(a1 +a9), 5/3*np.sqrt(2/3)*gamma**2/k1*a6,
+                    - gamma**2/np.sqrt(6)/k1*a7 - alfa*gamma*beta/np.sqrt(6)/k1/k3*a8, 5/3*np.sqrt(2/3)*gamma**2/k1*a4, - gamma**2/np.sqrt(6)/k1*a5, - alfa*gamma*beta/np.sqrt(6)/k1/k3*a5, - np.sqrt(3/2)*beta*gamma/k2*a3],
+
+                [0., 0., - (beta**2 + gamma**2)/Re, + 2/np.sqrt(6)*alfa*beta*gamma/k1/k2*a7 + (beta**2*(3*alfa**2+gamma**2) - 3*gamma**2*(alfa**2+gamma**2))/np.sqrt(6)/k1/k2/k3*a8, 2/np.sqrt(6)*alfa*beta*gamma/k1/k2*a6,
+                    2/np.sqrt(6)*alfa*beta*gamma/k1/k2*a5, 2/np.sqrt(6)*alfa*beta*gamma/k1/k2*a4, (beta**2*(3*alfa**2+gamma**2) - 3*gamma**2*(alfa**2+gamma**2))/np.sqrt(6)/k1/k2/k3*a4, 0.],
+
+                [- alfa/np.sqrt(6)*a5, - 10/3/np.sqrt(6)*alfa**2/k1*a6, - np.sqrt(3/2)*alfa*beta*gamma/k1/k2*a7 - np.sqrt(3/2)*alfa**2*beta**2/k1/k2/k3*a8, - (3*alfa**2 + 4*beta**2)/3/Re,
+                    - alfa/np.sqrt(6)*a1 - alfa/np.sqrt(6)*a9, - 10/3/np.sqrt(6)*alfa**2/k1*a2, - np.sqrt(3/2)*alfa*beta*gamma/k1/k2*a3, - np.sqrt(3/2)*alfa**2*beta**2/k1/k2/k3*a3, - alfa/np.sqrt(6)*a5],
+
+                [alfa/np.sqrt(6)*a4, + alfa**2/np.sqrt(6)/k1*a7 - alfa*beta*gamma/np.sqrt(6)/k1/k3*a8, 2/np.sqrt(6)*alfa*beta*gamma/k1/k2*a6, alfa/np.sqrt(6)*(a1 + a9), - (alfa**2 + beta**2)/Re,
+                    2/np.sqrt(6)*alfa*beta*gamma/k1/k2*a3, alfa**2/np.sqrt(6)/k1*a2, - alfa*beta*gamma/np.sqrt(6)/k1/k3*a2, alfa/np.sqrt(6)*a4],
+
+                [alfa/np.sqrt(6)*a7 + np.sqrt(3/2)*beta*gamma/k3*a8, 10/3/np.sqrt(6)*(alfa**2 - gamma**2)/k1*a4, - 2*np.sqrt(2/3)*alfa*beta*gamma/k1/k2*a5, 10/3/np.sqrt(6)*(alfa**2 - gamma**2)/k1*a2,
+                    - 2*np.sqrt(2/3)*alfa*beta*gamma/k1/k2*a3, - (3*alfa**2 + 4*beta**2 + 3*gamma**2)/3/Re, alfa/np.sqrt(6)*(a1 + a9), np.sqrt(3/2)*beta*gamma/k3*(a1+a9), alfa/np.sqrt(6)*a7 + np.sqrt(3/2)*beta*gamma/k3*a8],
+
+                [- alfa/np.sqrt(6)*a6, (gamma**2 - alfa**2)/np.sqrt(6)/k1*a5, alfa*beta*gamma/np.sqrt(6)/k1/k2*a4, alfa*beta*gamma/np.sqrt(6)/k1/k2*a3, (gamma**2 - alfa**2)/np.sqrt(6)/k1*a2,
+                    - alfa/np.sqrt(6)*(a1 + a9), - k3**2/Re, 0., - alfa/np.sqrt(6)*a6],
+
+                [0., 2/np.sqrt(6)*alfa*beta*gamma/k3/k1*a5, gamma**2*(3*alfa**2 - beta**2 + 3*gamma**2)/np.sqrt(6)/k1/k2/k3*a4, gamma**2*(3*alfa**2 - beta**2 + 3*gamma**2)/np.sqrt(6)/k1/k2/k3*a3,
+                    2/np.sqrt(6)*alfa*beta*gamma/k3/k1*a2, 0., 0., - k3**2/Re, 0.],
+
+                [0., np.sqrt(3/2)*beta*gamma/k2*a3, np.sqrt(3/2)*beta*gamma/k2*a2, 0., 0., - np.sqrt(3/2)*beta*gamma/k3*a8, 0., - np.sqrt(3/2)*beta*gamma/k3*a6, - 9*beta**2/Re]
+                ])
+        return J
+
+
     def gen_data_lorenz63(self):
         q         = np.zeros((self.N_t,self.tot_steps-self.N_transient,self.dim))
 
@@ -155,3 +230,98 @@ class Systems(Solvers):
         q       = np.array(hf.get('q'))
         hf.close()
         return(q)
+
+
+    def solve_system_LEs(self,sys, N, Ntherm, u0, dim, norm_time=1):
+        """
+            Solves the ODEs for N time steps starting from u0.
+            Additionally it computes the Lyapunov spectrum and CLVs
+            Returned values are normalized.
+
+            Args:
+                system: str that defines the dynamical system
+                N: number of time steps
+                Ntherm: number of time steps for initial transient.
+                dt: timestep
+                u0: initial condition
+                saveclvs: flag to select if to save full CLVs or do a consistency check
+                norm_time: modulo of QR decomposition
+                params: parameters for ODE
+                subspace_LEs_indeces: for high dimensional systems D>3 the ordering of CLVs in subspaces.
+            Returns:
+                normalized time series of shape (N+1, u0.size)
+        """
+
+        print('Dynamical system:',sys)
+        if sys=='lorenz63':
+            eom = self.solve_lorenz63
+            jac = self.solve_lorenz63_jac
+
+        if sys=='lorenz96':
+            eom = self.solve_lorenz96
+            jac = self.solve_lorenz96_jac
+
+        if sys=='MFE':
+            eom = self.solve_MFE
+            jac = self.solve_MFE_jac
+
+        #Timesteps in test set
+        N_test = N - Ntherm
+
+        T = np.arange(N+1) * self.dt
+        Ttest = np.arange(1,int((N_test)/norm_time)+1) * self.dt * norm_time
+        xt = u0
+        Xt = np.empty((T.size, u0.size))
+        Xt[0] = u0
+
+        N_test_norm = int(N_test/norm_time)
+
+        # Lyapunov Exponents timeseries
+        LE   = np.zeros((N_test_norm,dim))
+        # finite-time Lyapunov Exponents timeseries
+        FTLE = np.zeros((N_test_norm,dim))
+        # Q matrix recorded in time
+        QQ_t = np.zeros((dim,dim,N_test_norm))
+        # R matrix recorded in time
+        RR_t = np.zeros((dim,dim,N_test_norm))
+
+        #set random orthonormal Lyapunov vectors (GSVs)
+        U    = scipy.linalg.orth(np.random.rand(dim,dim))#np.eye(dim)
+        Q, R = self.qr_factorization(U)
+        U    = Q[:,:dim]
+
+        indx = 0
+        for i in range(1, T.size):
+            # xt0       = xt
+            xn, Mtemp = self.RK4var(self.F_all, eom, jac, xt, U)
+            xt       += xn
+            U        += Mtemp
+
+            if i%norm_time==0:
+
+                Q, R = self.qr_factorization(U)
+                U    = Q[:,:dim]
+                if i > Ntherm:
+                    QQ_t[:,:,indx] = Q
+                    RR_t[:,:,indx] = R
+                    LE[indx]       = np.abs(np.diag(R))
+                    # Jacobian       = jac(xt, params)
+                    FTLE[indx]     = (1./self.dt)*np.log(LE[indx])
+                    indx          += 1
+
+            Xt[i] = xt
+
+
+        LEs = np.cumsum(np.log(LE[:]),axis=0) / np.tile(Ttest[:],(dim,1)).T
+
+        # #Calculation of CLVs
+        # if check_clv == 1:
+        #     print("Calculate CLVs")
+        #     CLV_calculation(system,params,None,None,QQ_t,RR_t,dim,\
+        #                     Ntherm,dim,False,dt*norm_time,fname,\
+        #                     Xt, None, norm_time, saveclvs,LEs,\
+        #                     FTLE, subspace_LEs_indeces)
+        # #avFTLES = np.average(FTLE,axis=0)
+        # #print("average FTLEs",avFTLES)
+
+        return LEs , Xt
